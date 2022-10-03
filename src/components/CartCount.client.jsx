fcd7178ca4a33c7a0a9f95fc0b02a1fdc21b25f3
JUan Andrade <jfandradea@gmail.com>
2022-10-03 17:43:54 -0500

    practice in/out transitions

--- src/components/CartCount.client.jsx
+++ src/components/CartCount.client.jsx
@@ -1,19 +1,22 @@
 import { useCart } from "@shopify/hydrogen";
+import { useRef } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function Count() {
   const { totalQuantity } = useCart();
+  const nodeRef = useRef(null);
   return (
     <span className="cart-count-animation">
-      <TransitionGroup>
+      <TransitionGroup component={null}>
         <CSSTransition
+          nodeRef={nodeRef}
           unmountOnExit
           className="count"
           classNames="count"
           key={totalQuantity}
           timeout={{ enter: 400, exit: 400 }}
         >
-          <div>{totalQuantity}</div>
+          <div ref={nodeRef}>{totalQuantity}</div>
         </CSSTransition>
       </TransitionGroup>
     </span>
