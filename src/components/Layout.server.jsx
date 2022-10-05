355230aa086e21ce8896e61d4e9dbccdca6a0171
JUan Andrade <jfandradea@gmail.com>
2022-10-05 14:45:12 -0500

    set default seo component

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -1,9 +1,11 @@
 import { Suspense } from "react";
+import { DefaultSeo } from "./DefaultSeo.server";
 import { Header } from "./Header.server";
 
 export function Layout({ children }) {
   return (
     <div>
+      <DefaultSeo />
       <Header></Header>
       <main className="my-0 mx-auto p-10 max-w-5xl">
         <Suspense fallback={<AppFallback />}>{children}</Suspense>
