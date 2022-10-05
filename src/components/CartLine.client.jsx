7d05ffcbde7b1ed3bfd1f6b92009ff1c34cb838e
JUan Andrade <jfandradea@gmail.com>
2022-10-04 19:44:26 -0500

    applyiing transitions to amount changes in cart

--- src/components/CartLine.client.jsx
+++ src/components/CartLine.client.jsx
@@ -4,6 +4,8 @@ import {
   useCart,
   CartLineQuantityAdjustButton,
 } from "@shopify/hydrogen";
+import { useRef } from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function RemoveFromCart({ lineId }) {
   const { linesRemove } = useCart();
@@ -22,6 +24,7 @@ function RemoveFromCart({ lineId }) {
 
 function CartLine() {
   const { cost, merchandise, quantity, id } = useCartLine();
+  const nodeRef = useRef();
 
   return (
     <li className="py-4 px-0 border-b border-neutral-400 grid cart-line">
@@ -29,13 +32,28 @@ function CartLine() {
       <div className="flex flex-col justify-start line-product-details">
         <h3 className="m-0">{merchandise.product.title}</h3>
         <h4 className="m-0">{merchandise.title}</h4>
-        <p>
-          {cost.totalAmount.amount} {cost.totalAmount.currencyCode}
-        </p>
+
+        <div>
+          <TransitionGroup>
+            <CSSTransition
+              nodeRef={nodeRef}
+              unmountOnExit
+              className="line-quantity"
+              classNames="line-quantity"
+              key={quantity}
+              timeout={{ enter: 400, exit: 400 }}
+            >
+              <span>{cost.totalAmount.amount}</span>
+            </CSSTransition>
+
+            {cost.totalAmount.currencyCode}
+          </TransitionGroup>
+        </div>
         <div>
           <p className="mb-2">
             {quantity} &times; {merchandise.priceV2.amount} each
           </p>
+
           <div className="flex gap-2">
             <CartLineQuantityAdjustButton
               className="w-6 h-6 py-2  flex rounded-full bg-zinc-800 text-white items-center justify-center text-sm"
